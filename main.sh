22ce0d5a63772c2323398f8f789624257eaa20d5
chuyua <979093525@qq.com>
2022-04-26 14:07:25 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,13 @@
 # rm -rf alist* data/ #Uncomment this line to update
 if [ ! -f "artalk-go" ];then
-curl -L -O https://github.com/ArtalkJS/ArtalkGo/releases/download/v2.1.2/artalk-go_v2.1.2_linux_amd64.tar.gz
-tar -zxvf artalk-go_v2.1.2_linux_amd64.tar.gz
-rm -f artalk-go_v2.1.2_linux_amd64-go_v2.1.2_linux_amd64
-mv -b artalk-go_v2.1.2_linux_amd64/* ./ 
-mv -b artalk-go_v2.1.2_linux_amd64/.[^.]* ./ 
+curl -L  -o artalk.tar.gz https://github.com/ArtalkJS/ArtalkGo/releases/download/v2.1.4/artalk-go_v2.1.4_linux_amd64.tar.gz
+tar -zxvf artalk.tar.gz
+mv artalk-go_v2.1.4_linux_amd64.tar.gz artalk
+mv -b artalk/* ./ 
+mv -b artalk/.[^.]* ./ 
 rm -rf *~ 
-rm -rf artalk-go_v2.1.2_linux_amd64
+rm -rf artalk.tar.gz
+rm -rf artalk
 fi
+./artalk-go upgrade
 ./artalk-go serve
